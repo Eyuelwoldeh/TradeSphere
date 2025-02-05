ef03ecb6f5a5d17c9c682afd9d6046fa90ef06b6
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-05 01:31:30 -0600

    Initial commit

---
+++ src/main/java/com/tradeplatform/tradeanalytics/TradeanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.tradeplatform.tradeanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeanalyticsApplication.class, args);
+	}
+
+}
