00ceb6c358a411e897da7340a28c962bc0a90fee
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-15 19:52:41 -0600

    Allowed access from deployed frontend

--- src/main/java/com/tradeplatform/tradeanalytics/config/CorsConfig.java
+++ src/main/java/com/tradeplatform/tradeanalytics/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "https://trade-sphere-front-end-tsx.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://trade-sphere-front-end-tsx.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
