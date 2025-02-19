7b0c37e1d51599b9c04069f18eb4dfa43c948feb
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-19 13:02:11 -0600

    CORS config - updated to support less ports

--- src/main/java/com/tradeplatform/tradeanalytics/config/CorsConfig.java
+++ src/main/java/com/tradeplatform/tradeanalytics/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://trade-sphere-front-end-tsx.vercel.app")
+                .allowedOrigins("https://trade-sphere-front-end-3dnp5xuf4-eyuelwoldehs-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
