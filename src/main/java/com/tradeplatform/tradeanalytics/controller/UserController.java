803020078e23445445232b254cf67f86bf87d239
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-20 20:25:50 -0600

    Fingers Crossed .. Added Profile getter elements

--- src/main/java/com/tradeplatform/tradeanalytics/controller/UserController.java
+++ src/main/java/com/tradeplatform/tradeanalytics/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.tradeplatform.tradeanalytics.controller;
 
 
+import com.tradeplatform.tradeanalytics.dto.UserDTO;
 import com.tradeplatform.tradeanalytics.model.User;
 import com.tradeplatform.tradeanalytics.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -17,10 +21,23 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers()
-    {
-        return new ResponseEntity<List<User>>(userService.allUsers(), HttpStatus.OK);
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails == null) {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+
+        // Find user in MongoDB
+        Optional<User> user = userService.findByEmail(userDetails.getUsername()); // get username actually just returns the email ... because email is used as unique identifier for login
+        if (!user.isPresent()) {
+            return ResponseEntity.status(404).body("User not found");
+        }
+
+        UserDTO userDTO = new UserDTO(user.get().getUid(),user.get().getEmail(),user.get().getName());
+
+        // Send user info (without password)
+        return ResponseEntity.ok(userDTO);
     }
 
     public static Long generateID() {
---
+++ src/main/java/com/tradeplatform/tradeanalytics/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.tradeplatform.tradeanalytics.dto;
+
+
+// I am using this class because I dont want to pass sensitive info to the reposnse when I load the user profile..
+public class UserDTO {
+    private String id;
+    private String email;
+    private String name;
+
+    public UserDTO(String id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    // Getters
+    public String getId() { return id; }
+    public String getEmail() { return email; }
+    public String getName() { return name; }
+}
